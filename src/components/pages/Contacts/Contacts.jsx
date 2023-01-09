1083cd909dcf6f5e111bdce7f84c0e386c5d5e03
InnaShtondenko <inna.mycase@gmail.com>
2023-01-09 22:43:11 +0200

    Create Contacts.jsx

---
+++ src/components/pages/Contacts/Contacts.jsx
@@ -0,0 +1,27 @@
+import { ContactForm, ContactList, Filter } from 'components/AllComponents';
+
+import { Box } from 'components/common/Box/Box.styled';
+import { ContainerCardCommon } from 'components/common/shared.styled';
+
+export function Contacts() {
+  return (
+    <ContainerCardCommon>
+      <Box color="textColored">
+        <h1>Phonebook</h1>
+        <ContactForm />
+
+        <Box
+          width={0.85}
+          margin="0 auto"
+          mt={[4]}
+          borderColor="accentSecondary"
+          color="textColoredSecondary"
+        >
+          <h2>Contacts</h2>
+          <Filter />
+          <ContactList />
+        </Box>
+      </Box>
+    </ContainerCardCommon>
+  );
+}
