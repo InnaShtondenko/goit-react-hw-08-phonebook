d033e621d4deb846e4aa1ea5d2d08679f4fa25f6
InnaShtondenko <inna.mycase@gmail.com>
2023-01-09 22:43:20 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { phonebookAPI } from "./contactsAPISlice";
-
-import { filterReducer } from "./filterSlice"; 
-
+import persistStore from "redux-persist/es/persistStore";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { persistedAuthReducer } from "./slices/authSlice.js";
+import { phonebookAPI } from "./slices/contactsAPISlice.js";
+import { filterReducer } from "./slices/filterSlice"; 
+import { usersAPI } from "./slices/usersAPISlice.js";
 
 export const store = configureStore({
   reducer: {
     [phonebookAPI.reducerPath]: phonebookAPI.reducer,
+    [usersAPI.reducerPath]: usersAPI.reducer,
     filter: filterReducer,
+    auth: persistedAuthReducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(phonebookAPI.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      }
+    })
+      .concat(phonebookAPI.middleware)
+      .concat(usersAPI.middleware),
 });
+
+export const persistedStore = persistStore(store);
