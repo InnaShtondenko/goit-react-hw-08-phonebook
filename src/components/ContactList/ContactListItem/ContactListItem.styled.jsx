39d8d1a8e13d55e46fd1f493afa63f761c6c0f7f
InnaShtondenko <inna.mycase@gmail.com>
2023-01-09 22:43:09 +0200

    Update ContactListItem.styled.jsx

--- src/components/ContactList/ContactListItem/ContactListItem.styled.jsx
+++ src/components/ContactList/ContactListItem/ContactListItem.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { activeButton } from 'components/utils/Shared'; 
+
 
 export const ContactInfo = styled.div`
   font-size: 16px;
@@ -10,13 +10,4 @@ export const ContactInfo = styled.div`
   color: ${p => p.theme.colors.dark};
   text-align: left;
   word-break: break-all;
-`;
-
-export const DeleteButton = styled.button`
-  ${activeButton};
-  font-size: ${p => p.theme.fontSizes[2]}px;
-  font-weight: ${p => p.theme.fontWeights.bold};
-  padding: ${p => p.theme.space[1]}px ${p => p.theme.space[3]}px;
-  border-radius: ${p => p.theme.radius.normal};
-  box-shadow: ${p => p.theme.shadows.buttonInset};
 `;
