5e87af19ef7f9999ef7a46321983682ee905c0e1
InnaShtondenko <inna.mycase@gmail.com>
2023-06-02 00:01:59 +0300

    Create Register.jsx

---
+++ src/components/pages/Register/Register.jsx
@@ -0,0 +1,74 @@
+import { Notify } from 'notiflix';
+
+import { useSignupUserMutation } from 'redux/slices/usersAPISlice';
+
+import {
+  Box,
+  ButtonWideCommon,
+  ContainerCardCommon,
+  FormCommon,
+  InputCommon,
+  InputInfoLabelCommon,
+} from 'components/common/shared.styled';
+
+export function Register() {
+  const [sendSignupRequest, { isLoading }] = useSignupUserMutation();
+  const registerUser = async userCredentials => {
+    try {
+      const {
+        user: { name: userName },
+      } = await sendSignupRequest(userCredentials).unwrap();
+      Notify.success(
+        `You have been successfully registered! Welcome ${userName} 🥳`,
+        undefined,
+        { position: 'left-top' }
+      );
+    } catch (err) {
+      console.log(err);
+      Notify.failure(`Registration failed! Please try again.`);
+    }
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const {
+      name: { value: name },
+      email: { value: email },
+      password: { value: password },
+    } = e.target.elements;
+    const userCredentials = { name, email, password };
+
+    registerUser(userCredentials);
+  };
+
+  return (
+    <ContainerCardCommon>
+      <Box color="textColor">
+        <h2>Register</h2>
+
+        <FormCommon onSubmit={onSubmit}>
+          <InputInfoLabelCommon>
+            Name
+            <InputCommon type="text" name="name" autoComplete="on" autoFocus />
+          </InputInfoLabelCommon>
+          <InputInfoLabelCommon>
+            Email
+            <InputCommon type="email" name="email" autoComplete="on" />
+          </InputInfoLabelCommon>
+          <InputInfoLabelCommon>
+            Password
+            <InputCommon
+              type="password"
+              name="password"
+              minLength="5"
+              maxLength="12"
+            />
+          </InputInfoLabelCommon>
+          <ButtonWideCommon type="submit" disabled={isLoading}>
+           REGISTER
+          </ButtonWideCommon>
+        </FormCommon>
+      </Box>
+    </ContainerCardCommon>
+  );
+}
