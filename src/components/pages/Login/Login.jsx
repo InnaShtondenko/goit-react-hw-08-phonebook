a258267d03c923707107c9a76982152235174590
InnaShtondenko <inna.mycase@gmail.com>
2023-06-02 00:01:59 +0300

    Create Login.jsx

---
+++ src/components/pages/Login/Login.jsx
@@ -0,0 +1,75 @@
+import { Notify } from 'notiflix';
+
+import { useLoginUserMutation } from 'redux/slices/usersAPISlice';
+
+import {
+  Box,
+  ButtonWideCommon,
+  ContainerCardCommon,
+  FormCommon,
+  InputCommon,
+  InputInfoLabelCommon,
+} from 'components/common/shared.styled';
+
+export function Login() {
+  const [sendLoginRequest, { isLoading }] = useLoginUserMutation();
+  const loginUser = async userCredentials => {
+    try {
+      const {
+        user: { name: userName },
+      } = await sendLoginRequest(userCredentials).unwrap();
+
+      Notify.success(
+        `You have been successfully login! Welcome ${userName} 🥳`,
+        undefined,
+        { position: 'left-top' }
+      );
+    } catch (err) {
+      console.log(err);
+      Notify.failure(`Login failed! Please try again.`);
+    }
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const {
+      email: { value: email },
+      password: { value: password },
+    } = e.target.elements;
+    const userCredentials = { email, password };
+
+    loginUser(userCredentials);
+  };
+
+  return (
+    <ContainerCardCommon>
+      <Box color="textColor">
+        <h2>Login</h2>
+
+        <FormCommon onSubmit={onSubmit}>
+          <InputInfoLabelCommon>
+            Email
+            <InputCommon
+              type="email"
+              name="email"
+              autoComplete="on"
+              autoFocus
+            />
+          </InputInfoLabelCommon>
+          <InputInfoLabelCommon>
+            Password
+            <InputCommon
+              type="password"
+              name="password"
+              minLength="5"
+              maxLength="12"
+            />
+          </InputInfoLabelCommon>
+          <ButtonWideCommon type="submit" disabled={isLoading}>
+            Log me in!
+          </ButtonWideCommon>
+        </FormCommon>
+      </Box>
+    </ContainerCardCommon>
+  );
+}
