45e5deb3d8885187ad2863e1a883b3b05714ed6c
InnaShtondenko <inna.mycase@gmail.com>
2023-06-02 00:02:09 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,18 +1,29 @@
 import axios from 'axios';
 
+export const AUTH_HEADER_URL = 'Authorization';
+
 export const axiosBaseQuery =
-  ({ baseUrl } = { baseUrl: '' }) =>
-  async ({ url = '', method, data, params }) => {
-    try {
-      const result = await axios({ url: baseUrl + url, method, data, params });
-      return { data: result.data };
-    } catch (axiosError) {
-      let err = axiosError;
-      return {
-        error: {
-          status: err.response?.status,
-          data: err.response?.data || err.message,
-        },
-      };
-    }
+  ({ baseUrl, prepareHeaders } = { baseUrl: '' }
+  ) => {
+    const headers = new Headers();
+  
+    return async ({ url = '', method, data, params }, api) => {
+      prepareHeaders(headers, api);
+      axios.defaults.headers.common[AUTH_HEADER_URL] =
+        headers.get(AUTH_HEADER_URL) || '';
+
+      try {
+        const result = await axios({ url: baseUrl + url, method, data, params });
+      
+        return { data: result.data };
+      } catch (axiosError) {
+        let err = axiosError;
+        return {
+          error: {
+            status: err.response?.status,
+            data: err.response?.data || err.message,
+          },
+        };
+      }
+    };
   };
